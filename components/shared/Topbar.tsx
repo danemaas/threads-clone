92de1878a5488493ce2e191fb47690fe336c45ae
danemaas <danemaas16@gmail.com>
2023-10-29 22:37:06 +0800

    added some dependencies and functionality

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 function Topbar() {
   return (
@@ -28,6 +29,7 @@ function Topbar() {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
